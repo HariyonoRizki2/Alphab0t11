54fa6c6aee9124e19aadb92e44da73a2994bf6e2
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2021-11-23 09:07:37 +0700

    Update lol.js

--- shop/lol.js
+++ shop/lol.js
@@ -1,5 +1,5 @@
 const list_lol = (prefix) => {
-return `*ZEEONE OFC さ*
+return `*KIVIRTUAL STORE*
 				
 PROSES	1-180Mnt 1X24 Jam			
 OPEN	10.00-19.00			
@@ -10,12 +10,12 @@ WC LOLM
 125WC	Rp 18,000.00			
 420WC	Rp 55,000.00			
 700WC	Rp 85,000.00			
-1.375WC	Rp 155,000.00			
-2.400WC	Rp 250,000.00			
-4,000.00WC	Rp 395,000.00			
-8.150WC	Rp 785,000.00			
+1375WC	Rp 155,000.00			
+2400WC	Rp 250,000.00			
+4000WC	Rp 395,000.00			
+8150WC	Rp 785,000.00			
 				
-Untuk pay / pembayan silahkan ketik ${prefix}pay
+Untuk Pembayaran, Silahkan Ketik ${prefix}pay
 `
 	}
 
