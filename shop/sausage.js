e28efd77336aac319067a793234799437bb67ebe
HARIYONO RIZKI <69133831+HariyonoRizki2@users.noreply.github.com>
2021-11-23 09:10:45 +0700

    Update sausage.js

--- shop/sausage.js
+++ shop/sausage.js
@@ -1,5 +1,5 @@
 const list_sausage = (prefix) => {
-return `*ZEEONE OFC さ*
+return `*KIVIRTUAL STORE*
 	
 PROSES	1-60Mnt Max 1X24 Jam		
 OPEN	09.00-21.00		
@@ -11,12 +11,12 @@ CANDY SAUSAGE
 180C	Rp 45,000.00		
 316C	Rp 68,000.00		
 718C	Rp 135,000.00		
-1.368C	Rp 265,000.00		
-2.118C	Rp 418,000.00		
-3.548C	Rp 682,000.00		
-7.096C	Rp 1.305,000.00		
+1368C	Rp 265,000.00		
+2118C	Rp 418,000.00		
+3548C	Rp 682,000.00		
+7096C	Rp 1.305,000.00		
 			
-Untuk pay / pembayan silahkan ketik ${prefix}pay
+Untuk Pembayaran, Silahkan Ketik ${prefix}pay
 `
 	}
 
